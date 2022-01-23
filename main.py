627f1193ea5399b76806f223d6506bba0107994f
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2022-01-23 15:26:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import flask
 import random
 import socket
-import tqdm
+#import tqdm
 import os
-from flask import render_template,url_for
+from flask import render_template,url_for,request
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
@@ -13,30 +13,34 @@ HOST = '127.0.0.1'
 PORT = random.randint(1024,49151)
 
 filename = "home.html"
-filesize = os.path.getsize(filename)
+#filesize = os.path.getsize(filename)
 
-s = socket.socket()
-addr = (HOST,PORT)
-
-s.bind(addr)
-s.listen(5)
-
-while True:
-    c,(c_h,c_p) = s.accept()
-    c.send(f"{filename}{SEPARATOR}{filesize}".encode())
+##s = socket.socket()
+##addr = (HOST,PORT)
+##
+##s.bind(addr)
+##s.listen(5)
+##
+##while True:
+##    c,(c_h,c_p) = s.accept()
+##    c.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
 @app.route('/')
 def home():
     return render_template('home.html') #Create game -> redirect to lobby
 
-@app.route('/lobby')
+@app.route('/lobby.html',methods = ["GET","POST"])
 def lobby():
     code = random.randint(0,1023) #port number
-    return render_template('lobby.html',code= PORT)
+    if request.method == "GET":
+        return render_template('lobby.html',code= PORT)
+    if 'number' in request.form:
+        number = request.form['number']
+        return render_template("setup.html")
+    
 
     
 
 
 if __name__ == "__main__":
     app.run()
-
