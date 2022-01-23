956f24ac6ba898b970c7b1b440cf771ca83231a8
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2022-01-23 13:35:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import flask
 import random
-from flask import render_template
+import socket
+import tqdm
+import os
+from flask import render_template,url_for
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
 
 app = flask.Flask(__name__)
+HOST = '127.0.0.1'
+PORT = random.randint(1024,49151)
+
+filename = "home.html"
+filesize = os.path.getsize(filename)
+
+s = socket.socket()
+addr = (HOST,PORT)
+
+s.bind(addr)
+s.listen(5)
+
+while True:
+    c,(c_h,c_p) = s.accept()
+    c.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') #Create game -> redirect to lobby
+
+@app.route('/lobby')
+def lobby():
+    code = random.randint(0,1023) #port number
+    return render_template('lobby.html',code= PORT)
+
+    
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
